437358d5ee3be827c776c235514846834f99384c
Grupo5CicloWeb <cicloc504@gmail.com>
2024-04-06 14:35:03 -0500

    ejercicio terminado

---
+++ src/main/java/pe/edu/cibertec/appformularios/model/ImcModel.java
@@ -0,0 +1,11 @@
+package pe.edu.cibertec.appformularios.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ImcModel {
+    private double peso;
+    private double talla;
+}
